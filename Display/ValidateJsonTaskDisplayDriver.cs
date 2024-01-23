512b7c17899dbf91a870de427295082995ab3ddc
Samuel Hembrow <samuelhembrow@gmail.com>
2024-01-23 15:53:26 +1300

    Validate JSON workflow task

---
+++ Display/ValidateJsonTaskDisplayDriver.cs
@@ -0,0 +1,28 @@
+﻿using OrchardCore.Workflows.Display;
+using OrchardCore.Workflows.Models;
+using PropertyBrokers.OrchardCore.WorkflowAdditions.ValidateJson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyBrokers.OrchardCore.WorkflowAdditions.Display
+{
+    public class ValidateJsonTaskDisplayDriver : ActivityDisplayDriver<ValidateJson.ValidateJsonTask, ValidateJsonTaskViewModel>
+    {
+        protected override void EditActivity(ValidateJson.ValidateJsonTask activity, ValidateJsonTaskViewModel model)
+        {
+            model.JsonContent = activity.JsonContent.Expression;
+            model.SchemaContent = activity.SchemaContent.Expression;
+            model.SchemaValidationState = activity.SchemaValidationState.Expression;
+        }
+
+        protected override void UpdateActivity(ValidateJsonTaskViewModel model, ValidateJson.ValidateJsonTask activity)
+        {
+            activity.JsonContent = new WorkflowExpression<string>(model.JsonContent);
+            activity.SchemaContent = new WorkflowExpression<string>(model.SchemaContent);
+            activity.SchemaValidationState = new WorkflowExpression<string>(model.SchemaValidationState);
+        }
+    }
+}
---
+++ ValidateJsonTask/ValidateJsonTask.cs
@@ -0,0 +1,98 @@
+﻿using Fluid.Parser;
+using Microsoft.Extensions.Localization;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using OrchardCore.ContentManagement;
+using OrchardCore.ContentManagement.Records;
+using OrchardCore.DisplayManagement.ModelBinding;
+using OrchardCore.Workflows.Abstractions.Models;
+using OrchardCore.Workflows.Activities;
+using OrchardCore.Workflows.Models;
+using OrchardCore.Workflows.Services;
+using PropertyBrokers.OrchardCore.WorkflowAdditions.EmailFile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YesSql;
+
+namespace PropertyBrokers.OrchardCore.WorkflowAdditions.ValidateJson
+{
+    public class ValidateJsonTask : TaskActivity
+    {
+        private readonly IWorkflowExpressionEvaluator _expressionEvaluator;
+        private readonly IStringLocalizer S;
+        private readonly ISession _session;
+        public ValidateJsonTask(
+            IWorkflowExpressionEvaluator expressionEvaluator,
+            ISession session,
+            IStringLocalizer<EmailFileTask> localizer
+        )
+        {
+            _expressionEvaluator = expressionEvaluator;
+            S = localizer;
+            _session = session;
+        }
+
+        public override string Name => nameof(ValidateJsonTask);
+        public override LocalizedString DisplayText => S["Validate JSON Task"];
+        public override LocalizedString Category => S["Validation"];
+
+        public WorkflowExpression<string> SchemaContent
+        {
+            get => GetProperty(() => new WorkflowExpression<string>());
+            set => SetProperty(value);
+        }
+
+        public WorkflowExpression<string> JsonContent
+        {
+            get => GetProperty(() => new WorkflowExpression<string>());
+            set => SetProperty(value);
+        }
+
+        public WorkflowExpression<string> SchemaValidationState
+        {
+            get => GetProperty(() => new WorkflowExpression<string>());
+            set => SetProperty(value);
+        }
+
+        public override IEnumerable<Outcome> GetPossibleOutcomes(WorkflowExecutionContext workflowContext, ActivityContext activityContext)
+        {
+            return Outcomes(S["Passed"], S["Failed"]);
+        }
+
+        public override async Task<ActivityExecutionResult> ExecuteAsync(WorkflowExecutionContext workflowContext, ActivityContext activityContext)
+        {
+            // Retrieve the schema content item
+            string schemaContent = await _expressionEvaluator.EvaluateAsync(SchemaContent, workflowContext, null);
+            JSchema schema = JSchema.Parse(schemaContent);
+
+            // JSON body
+            string contentStr = await _expressionEvaluator.EvaluateAsync(JsonContent, workflowContext, null);
+            JObject content;
+            try
+            {
+                content = JObject.Parse(contentStr);
+            } catch (Exception ex)
+            {
+                SchemaValidationState = new WorkflowExpression<string>("Json Body Parse Error: " + ex.Message);
+                return Outcomes("Failed");
+            }
+
+            List<string> errorMessages = new();
+            // Gets all errors with JSON body, not just the first
+            content.Validate(schema, (o, e) =>
+            {   
+                errorMessages.Add(e.Message);
+            });
+
+            if(errorMessages.Count > 0)
+            {
+                SchemaValidationState = new WorkflowExpression<string>("Json Body Parse Error: " + string.Join("\n", errorMessages));
+                return Outcomes("Failed");
+            }
+            return Outcomes("Passed");
+        }
+    }
+}
